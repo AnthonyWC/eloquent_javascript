70ac0c3b7a5157af80f22bf20719137c8ebfba2c
Anthony Cheng <anthony.wan.cheng@gmail.com>
2018-10-24 00:30:34 -0400

    Wed Oct 24 00:30:34 EDT 2018 by antho

--- chap3/src/min.js
+++ chap3/src/min.js
@@ -1,10 +1,12 @@
 'use strict';
 
+// input validation
 function isInt(value) {
   const er = /^(-?[1-9]+|-?\d+\.\d+)$/;
   return ((typeof value === 'number') && (er.test(value)));
 }
 
+// main
 function getmin(i, j) {
   if (isInt(i) && isInt(j)) {
     return Math.min(i, j);
@@ -12,6 +14,7 @@ function getmin(i, j) {
   return false;
 }
 
+// runtime check
 if (require.main === module) {
   try {
     console.log(getmin(8, 7));
